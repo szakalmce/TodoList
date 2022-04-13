538dfecb8d96fd4b05697b40f1fee7827d39f775
unknown <tomasz.garbarz9@gmail.com>
2022-04-13 07:49:09 +0200

    v.1.0.0

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,8 @@ import styles from "./Header.module.css";
 const Header = ({ tasks }) => {
   return (
     <div className={styles.wrapper}>
-      <p className={styles.flex}></p>
       <h1 className={styles.flex}>Simple Todo List</h1>
-      <p>Items todo : {tasks.length}</p>
+      <p className={styles.absolute}>Items todo : {tasks.length}</p>
     </div>
   );
 };
